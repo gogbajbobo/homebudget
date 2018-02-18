d700e19ddb2bccc9460e18156496dbc867949643
gogbajbobo <grigoriev.maxim@gmail.com>
2018-02-18 17:41:07 +0300

    add String+extension

---
+++ HomeBudget/Extensions/String+extension.swift
@@ -0,0 +1,25 @@
+//
+//  String+extension.swift
+//  HomeBudget
+//
+//  Created by Maxim Grigoriev on 18/02/2018.
+//  Copyright © 2018 Maxim Grigoriev. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    struct NumFormatter {
+        static let instance = NumberFormatter()
+    }
+    
+    var doubleValue: Double? {
+        return NumFormatter.instance.number(from: self)?.doubleValue
+    }
+    
+    var integerValue: Int? {
+        return NumFormatter.instance.number(from: self)?.intValue
+    }
+
+}
