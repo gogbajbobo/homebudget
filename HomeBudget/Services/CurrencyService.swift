42ea51ffb037dac3ef3880c2b9e5f8b71e9587bd
gogbajbobo <grigoriev.maxim@gmail.com>
2018-02-20 13:22:21 +0300

    add CurrencyService

---
+++ HomeBudget/Services/CurrencyService.swift
@@ -0,0 +1,81 @@
+//
+//  CurrencyService.swift
+//  HomeBudget
+//
+//  Created by Maxim Grigoriev on 20/02/2018.
+//  Copyright © 2018 Maxim Grigoriev. All rights reserved.
+//
+
+import Foundation
+
+struct CurrencyServiceError: Error {
+    
+    enum ErrorKind {
+        case dataIsNotDictionary
+    }
+    let kind: ErrorKind
+    
+}
+
+protocol CurrencyServiceProtocol {
+    
+    func getCurrencyRates(completionHandler: @escaping ([(String, Any)]?, Error?) -> Void)
+    func requestCurrencyRates(completionHandler: @escaping ([(String, Any)]?, Error?) -> Void)
+    
+}
+
+class CurrencyService: NSObject {
+
+    lazy var dateFormatter: DateFormatter = {
+        
+        $0.dateFormat = "yyyy-MM-dd"
+        return $0
+        
+    }(DateFormatter())
+    
+    func checkStoresCurrencyRates(completionHandler: @escaping ([(String, Any)]?, Error?) -> Void) {
+        
+        guard
+            let currRates = UserDefaults.standard.dictionary(forKey: "currRates"),
+            dateFormatter.string(from: Date()) == currRates["saveDate"] as? String,
+            var rates = currRates["rates"] as? Dictionary<String, Any>,
+            let base = currRates["base"] as? String else {
+                return requestCurrencyRates(completionHandler: completionHandler)
+        }
+        
+        rates[base] = 1
+        let returnValues = rates.sorted(by: { $0.0 < $1.0 })
+        completionHandler(returnValues, nil)
+
+    }
+    
+}
+
+extension CurrencyService: CurrencyServiceProtocol {
+    
+    func getCurrencyRates(completionHandler: @escaping ([(String, Any)]?, Error?) -> Void) {
+        checkStoresCurrencyRates(completionHandler: completionHandler)
+    }
+
+    func requestCurrencyRates(completionHandler: @escaping ([(String, Any)]?, Error?) -> Void) {
+        
+        NetworkService().requestCurrencyRates { (data, error) in
+            
+            guard var data = data as? Dictionary<String, Any> else {
+                return completionHandler(nil, CurrencyServiceError(kind: .dataIsNotDictionary))
+            }
+            
+            print(data)
+            
+            data["saveDate"] = self.dateFormatter.string(from: Date())
+            
+            UserDefaults.standard.set(data, forKey: "currRates")
+            UserDefaults.standard.synchronize()
+            
+            self.checkStoresCurrencyRates(completionHandler: completionHandler)
+            
+        }
+        
+    }
+
+}
