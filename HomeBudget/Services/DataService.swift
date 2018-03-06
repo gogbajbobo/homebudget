83b036dec382a66f62df044affe5c13987f3bd82
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-06 12:22:32 +0300

    changes in DataService.swift

--- HomeBudget/Services/DataService.swift
+++ HomeBudget/Services/DataService.swift
@@ -13,8 +13,10 @@ import CoreData
 
 protocol DataServiceProtocol {
     
-    func fetchedResultsController<T>(_ entityType: T.Type, sortDescriptors: [(key: String, asc: Bool)]) -> NSFetchedResultsController<T>
+    func fetchedResultsController<T: NSFetchRequestResult>(_ entityType: T.Type, sortDescriptors: [(key: String, asc: Bool)]) -> NSFetchedResultsController<T>
     
+    func fetchedResultsController<T: NSFetchRequestResult>(_ entityType: T.Type, sortDescriptors: [(key: String, asc: Bool)], predicate: NSPredicate?) -> NSFetchedResultsController<T>
+
 }
 
 
@@ -24,16 +26,24 @@ class DataService {
 
 extension DataService: DataServiceProtocol {
     
-    func fetchedResultsController<T>(_ entityType: T.Type, sortDescriptors: [(key: String, asc: Bool)]) -> NSFetchedResultsController<T> {
+    func fetchedResultsController<T: NSFetchRequestResult>(_ entityType: T.Type, sortDescriptors: [(key: String, asc: Bool)]) -> NSFetchedResultsController<T> {
     
+        return fetchedResultsController(entityType, sortDescriptors: sortDescriptors, predicate: nil)
+        
+    }
+    
+    func fetchedResultsController<T: NSFetchRequestResult>(_ entityType: T.Type, sortDescriptors: [(key: String, asc: Bool)], predicate: NSPredicate?) -> NSFetchedResultsController<T> {
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-
-        let fetchRequest: NSFetchRequest<T> = NSFetchRequest(entityName: String(describing: T.self))
+        
+        let fetchRequest: NSFetchRequest<T> = NSFetchRequest(entityName: String(describing: entityType.self))
         
         fetchRequest.sortDescriptors = sortDescriptors.map({ (key, asc) -> NSSortDescriptor in
             return NSSortDescriptor(key: key, ascending: asc)
         })
+        
+        fetchRequest.predicate = predicate
         
         let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
                                              managedObjectContext: context,
