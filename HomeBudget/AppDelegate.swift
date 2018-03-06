6ce9e4280ecb3bed1628325de8d94aff2b844f5a
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-06 15:37:57 +0300

    SocketIO.start()

--- HomeBudget/AppDelegate.swift
+++ HomeBudget/AppDelegate.swift
@@ -19,8 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        SocketIO.start()
+        
         return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
